4cc71c7403e242aa6f6878d550af63a8512eb1ad
Miraj-Mahmud-Babu <mahmud.babu1994@gmail.com>
2021-10-02 00:47:54 +0600

    v-5

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
 // new function
 
 document.querySelectorAll(".filter_icon").forEach(function (el) {
   el.addEventListener("click", function () {
-      console.log("miraj")
+    console.log("miraj");
     var element = el.closest(".filter_bar");
     element.classList.add("filter-visible");
   });
@@ -21,19 +19,50 @@ document.querySelectorAll(".cross-btn").forEach(function (el) {
 
 document.querySelectorAll(".search_icon").forEach(function (ev) {
   ev.addEventListener("click", function (ev) {
-      console.log("miraj")
+    console.log("miraj");
     var ele = ev.closest(".search-input-box-design");
     ele.classList.add("filter-visible");
   });
 });
 
-// document.querySelector(".icon").addEventListener("click", function(){
-//   document.querySelector(".search-input").classList.toggle("active");
-// })
+document.querySelectorAll(".search-icon").forEach(function (el) {
+  el.addEventListener("click", function () {
+    el.closest(".main-search-icon").classList.toggle("search-active");
+  });
+});
 
+document.querySelectorAll(".search-icon-header").forEach(function (ev) {
+  ev.addEventListener("click", function () {
+    ev.closest(".main-search-icon-header").classList.toggle(
+      "search-active-header"
+    );
+  });
+});
 
-document.querySelectorAll(".icon").forEach(function(el){
-  el.addEventListener("click", function(){
-   el.closest(".main-search-icon").classList.toggle("active");
-  })
-})
+// moduls item body search bar
+const modalsListItemsBody = document.querySelector(".search-input");
+console.log(modalsListItemsBody, this);
+
+document.querySelectorAll(".search-input").forEach(function (bev) {
+  document.addEventListener("click", function () {
+    if (!bev.target.closest(".main-search-icon")) {
+      modalsListItemsBody.style.visibility = "hidden";
+    } else {
+      modalsListItemsBody.style.visibility = "visible";
+    }
+  });
+});
+
+// moduls item header search bar
+const modalsListItems = document.querySelector(".search-input-header");
+// console.log(modalsListItems);
+
+document.querySelectorAll(".search-input").forEach(function () {
+  document.addEventListener("click", function (event) {
+    if (!event.target.closest(".main-search-icon-header")) {
+      modalsListItems.style.visibility = "hidden";
+    } else {
+      modalsListItems.style.visibility = "visible";
+    }
+  });
+});
