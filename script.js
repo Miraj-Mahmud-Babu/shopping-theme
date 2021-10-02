047179fbd65490a640f3f0ee5ef6a51247f63589
Miraj-Mahmud-Babu <mahmud.babu1994@gmail.com>
2021-10-02 15:29:53 +0600

    v-8

--- script.js
+++ script.js
@@ -43,16 +43,6 @@ document.querySelectorAll(".search-icon-header").forEach(function (ev) {
 const modalsListItemsBody = document.querySelector(".search-input");
 console.log(modalsListItemsBody, this);
 
-// document.querySelectorAll(".search-input").forEach(function (bev) {
-//   document.addEventListener("click", function () {
-//     if (!bev.target.closest(".main-search-icon")) {
-//       modalsListItemsBody.style.visibility = "hidden";
-//     } else {
-//       modalsListItemsBody.style.visibility = "visible";
-//     }
-//   });
-// });
-
 // moduls item header search bar
 const modalsListItems = document.querySelector(".search-input-header");
 // console.log(modalsListItems);
@@ -67,31 +57,29 @@ document.querySelectorAll(".search-input").forEach(function () {
   });
 });
 
-// document.querySelector("body").addEventListener("click", function(e) {
-//   console.log(e.target.classList)
-//   console.log(e.target.matches(".search-input span"))
-
-// })
-
-
-window.addEventListener('click', function(e) {
-  if (
-    !e.target.closest(".main-search-icon")
-  ) {
-
-    document.querySelectorAll(".main-search-icon").forEach(function(el)
-    {
-      el.classList.remove("search-active")
-    })
+window.addEventListener("click", function (e) {
+  if (!e.target.closest(".main-search-icon")) {
+    document.querySelectorAll(".main-search-icon").forEach(function (el) {
+      el.classList.remove("search-active");
+      
+    });
   }
 
-  if (
-    !e.target.closest(".position-filter-section") && !e.target.closest(".filter_icon")
-  ) {
-
-    document.querySelectorAll(".filter_bar").forEach(function(el)
-    {
-      el.classList.remove("filter-visible")
-    })
+  if (!e.target.closest(".main-search-icon-header")) {
+    document.querySelectorAll(".main-search-icon-header").forEach(function (el) {
+      el.classList.remove("search-active-header");
+      // el.classList.remove("search-icon-header");
+    });
   }
-})
+
+  
+
+  if (
+    !e.target.closest(".position-filter-section") &&
+    !e.target.closest(".filter_icon")
+  ) {
+    document.querySelectorAll(".filter_bar").forEach(function (el) {
+      el.classList.remove("filter-visible");
+    });
+  }
+});
